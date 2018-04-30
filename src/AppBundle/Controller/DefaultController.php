f2b9bde69c18660ab2d0f96832fab2de6e2419c8
pushpakumara.14@cse.mrt.ac.lk <pushpakumara.14@cse.mrt.ac.lk>
2018-04-30 18:55:00 +0530

    knp pagination is added

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -12,9 +12,6 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
     public function indexAction(Request $request)
     {
         $allData = $this->getDoctrine()
@@ -23,6 +20,26 @@ class DefaultController extends Controller
         return $this->render('pages/data.html.twig', array(
             'allData' => $allData
         ));
+    }
+
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function listAction(Request $request)
+    {
+        $em    = $this->get('doctrine.orm.entity_manager');
+        $dql   = "SELECT a FROM AppBundle:Data a";
+        $query = $em->createQuery($dql);
+
+        $paginator  = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $query, /* query NOT result */
+            $request->query->getInt('page', 1)/*page number*/,
+            10/*limit per page*/
+        );
+
+        // parameters to template
+        return $this->render('pages/list.html.twig', array('pagination' => $pagination));
     }
 
     /**
